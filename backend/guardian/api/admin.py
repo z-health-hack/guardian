ea50031ed1d5ec27f28116c1315b1dcc7f86ec3e
Bleuler, Cedric <cedric.bleuler@zuehlke.com>
2020-11-07 15:50:11 +0100

    Register Patient in admin interface

--- backend/guardian/api/admin.py
+++ backend/guardian/api/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from api.models import TimeSeries, DataPoint
+from api.models import TimeSeries, DataPoint, Patient
 
 admin.site.register(TimeSeries)
 admin.site.register(DataPoint)
+admin.site.register(Patient)
