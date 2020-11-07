1734743625fb82cea7a957c8eb66fdd86740b8ed
Bachmann, Benjamin <Benjamin.Bachmann@zuehlke.com>
2020-11-07 02:10:54 +0100

    Frontend: Remove auto logout on bad request.

--- frontend/guardian/src/app/auth/auth.inteceptor.ts
+++ frontend/guardian/src/app/auth/auth.inteceptor.ts
@@ -1,8 +1,7 @@
 import {Injectable} from '@angular/core';
 
 import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
-import {catchError} from 'rxjs/operators';
-import {Observable, throwError} from 'rxjs';
+import {Observable} from 'rxjs';
 import {Router} from '@angular/router';
 import {AuthService} from './auth.service';
 
@@ -25,12 +24,7 @@ export class AuthInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const authToken = this.authService.getAccessToken();
     const clonedReq = AuthInterceptor.cloneRequestWithAccessToken(request, authToken);
-    return next.handle(clonedReq).pipe(
-      catchError(err => {
-        this.logout();
-        return throwError(err);
-      })
-    );
+    return next.handle(clonedReq);
   }
 
   private logout(): void {
