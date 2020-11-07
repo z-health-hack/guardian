2e784344c0a90ad44c69c6c540877dd6169ec487
Bachmann, Benjamin <Benjamin.Bachmann@zuehlke.com>
2020-11-07 14:55:06 +0100

    Frontend: More cache control parameters.

--- frontend/guardian/src/app/auth/auth.inteceptor.ts
+++ frontend/guardian/src/app/auth/auth.inteceptor.ts
@@ -17,7 +17,7 @@ export class AuthInterceptor implements HttpInterceptor {
     return request.clone({
       setHeaders: {
         Authorization: 'Token ' + accessToken,
-        'Cache-Control': 'no-cache'
+        'Cache-Control': 'no-cache, no-store, must-revalidate'
       }
     });
   }
