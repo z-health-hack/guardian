fb6650ab73e6b0360644fe132857aa5680a8a61a
Bachmann, Benjamin <Benjamin.Bachmann@zuehlke.com>
2020-11-07 20:53:43 +0100

    Frontend: Logout on 403.

--- frontend/guardian/src/app/auth/auth.inteceptor.ts
+++ frontend/guardian/src/app/auth/auth.inteceptor.ts
@@ -1,9 +1,10 @@
 import {Injectable} from '@angular/core';
 
 import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
-import {Observable} from 'rxjs';
+import {Observable, throwError} from 'rxjs';
 import {Router} from '@angular/router';
 import {AuthService} from './auth.service';
+import {catchError} from "rxjs/operators";
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
@@ -25,7 +26,14 @@ export class AuthInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const authToken = this.authService.getAccessToken();
     const clonedReq = AuthInterceptor.cloneRequestWithAccessToken(request, authToken);
-    return next.handle(clonedReq);
+    return next.handle(clonedReq).pipe(
+      catchError(err => {
+        if (err.status === 403) {
+          this.logout();
+        }
+        return throwError(err);
+      })
+    );
   }
 
   private logout(): void {
