c60cc7b9d7fec012ac1389e637fadeca4a602a2c
Bachmann, Benjamin <Benjamin.Bachmann@zuehlke.com>
2020-11-07 11:10:30 +0100

    Frontend: Implement patients service and list.

--- frontend/guardian/src/app/patient/patient-list/patient-list.component.ts
+++ frontend/guardian/src/app/patient/patient-list/patient-list.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Subject} from 'rxjs';
+import {PatientsService} from '../patients.service';
+import {UserProfile} from '../../auth/auth.model';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-patient-list',
   templateUrl: './patient-list.component.html',
   styleUrls: ['./patient-list.component.css']
 })
-export class PatientListComponent implements OnInit {
+export class PatientListComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private unsubscribe$ = new Subject<void>();
 
-  ngOnInit(): void {
+  public patients: UserProfile[] = [];
+
+  constructor(
+    private router: Router,
+    private patientsService: PatientsService,
+  ) {
   }
 
+  ngOnInit(): void {
+    this.patientsService.getPatients()
+      .subscribe(patients => {
+        this.patients = patients;
+        if (patients.length === 1) {
+          this.router.navigate([`/patients/detail/${patients[0].id}`]);
+        }
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
 }
---
+++ frontend/guardian/src/app/patient/patients.service.ts
@@ -0,0 +1,19 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {PATIENTS_URL} from '../endpoints';
+import {UserProfile} from '../auth/auth.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PatientsService {
+
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  public getPatients(): Observable<UserProfile[]> {
+    return this.httpClient.get<UserProfile[]>(PATIENTS_URL);
+  }
+}
